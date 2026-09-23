f5e7345bea7d0d3cab4b80ca478a0a389060d702
MEbsen <35669569+MEbsen@users.noreply.github.com>
2026-09-23 17:40:17 +0200

    Prepare v0.2.0-dev.3

--- src/iroomba-s7-card.js
+++ src/iroomba-s7-card.js
@@ -1,4 +1,4 @@
-// Development builds are published automatically from this branch.
+// Development builds are published automatically from this branch. Activity moves around a stationary robot.
 const CARD_VERSION = "0.2.0-dev.3";
 
 const FEATURES = { PAUSE: 4, STOP: 8, RETURN_HOME: 16, LOCATE: 512, CLEAN_SPOT: 1024, START: 8192 };
